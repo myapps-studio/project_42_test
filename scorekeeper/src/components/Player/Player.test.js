b92c2d2dd750b85c2ac3a4d287a2231037b63672
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-09-11 19:44:43 +0200

    test

---
+++ scorekeeper/src/components/Player/Player.test.js
@@ -0,0 +1,50 @@
+import Player from './Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+
+it('renders correct name', () => {
+    const playerNamePassed = 'Ania';
+    const playerComponent = shallow(<Player name={playerNamePassed} />);
+  
+    const playerNameRendered = playerComponent.find('.Player__name').text();
+  
+    expect(playerNameRendered).toEqual(playerNamePassed);
+});
+
+// test dla score
+
+it('renders correct score', () => {
+    const playerScorePassed = '2';
+    const playerComponent = shallow(<Player score={playerScorePassed} />);
+  
+    const playerScoreRendered = Number(playerComponent.find('.Player__score').text());
+  
+    expect(playerScoreRendered).toEqual(playerScorePassed);
+});
+
+// test klikniecia w przycisk +
+
+it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+    const mockedOnPlayerScoreChange = jest.fn();
+    const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+  
+    const plusButton = playerComponent.find('.Player__button').first();
+  
+    plusButton.simulate('click');
+  
+    expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+  });
+
+  // test klikniecia w przycisk -
+
+  it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+    const mockedOnPlayerScoreChange = jest.fn();
+    const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+  
+    const plusButton = playerComponent.find('.Player__button').last();
+  
+    plusButton.simulate('click');
+  
+    expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+  });
